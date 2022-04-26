a198016d16c1f60ddf494d2da5a4838f133d5911
JDitto95 <powell0295@gmail.com>
2022-04-25 20:51:06 -0400

    chptr-2

---
+++ fiveTimes.py
@@ -0,0 +1,13 @@
+#sec block of code for fiveTimes.py
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times(' + str(i) + ')')
+    i = i+1
+
+
+
+#first block of code for fiveTimes.py
+# print('My name is')
+# for i in range(5):
+#     print('Jimmy Five Times (' + str(i) + ')')
---
+++ printRandom.py
@@ -0,0 +1,3 @@
+import random
+for I in range(5):
+    print(random.randint(1, 10))
